8a547449616fbb802ff0fa3c10c68d66c8578e81
Alexander.Isaev <JATragedian@yandex.ru>
2017-03-12 14:43:50 +0300

    Added show(int n), test function in main() that checks all our methods, deleted destructor;

--- jzQueue.h
+++ jzQueue.h
@@ -3,14 +3,16 @@
 
 template <class Qt> class Queue {
 public:
-	Queue(); // Constructor;
-	~Queue(); // Destructor;
-	void push(Qt x);
+	Queue(); // +
+	void push(Qt x); // +
 	void pop();
-	void show(); // We need to create an overload method here!;
-	Qt findMin();
+	void show(); // +
+	void show(int n); // +
+	void show(int *from, int *to);
+	Qt findMin(); // +
 	Qt findMax();
-	int size();
+	int size(); // +
+	int szof();
 	bool empty();
 	void qSort(); // QuickSort;
 
@@ -18,4 +20,88 @@ private:
 	Qt *queue;
 	int num;
 };
+
+////////////////////////// Methods ///////////////////////////
+
+template <class Qt>
+Queue<Qt>::Queue()
+{
+	num = 0;
+	queue = new Qt[];
+}
+
+template <class Qt>
+void Queue<Qt>::push(Qt x)
+{
+	queue[num] = x;
+	num++;
+}
+
+template <class Qt>
+Qt Queue<Qt>::findMin()
+{
+	if (num > 0) {
+		Qt x = queue[0];
+
+		if (num > 1) {
+			for (size_t i = 1; i < num; i++) {
+				if (x > queue[i]) x = queue[i];
+			}
+
+			return x;
+		}
+
+		else return x;
+	}
+
+	else return 0;
+}
+
+template <class Qt>
+int Queue<Qt>::size()
+{
+	return num;
+}
+
+template <class Qt>
+int Queue<Qt>::szof()
+{
+	unsigned long size;
+
+	size = sizeof(queue); // Doesn't showing the whole size!...
+	return size;
+}
+
+template <class Qt>
+void Queue<Qt>::show()
+{
+	if (num > 0) {
+		for (size_t i = 0; i < num; i++) {
+			std::cout << "Element " << i << ": " << queue[i] << std::endl;
+		}
+	}
+
+	else std::cout << "\nQueue is empty!\n";
+}
+
+template <class Qt>
+void Queue<Qt>::show(int n)
+{
+	if (num > 0) {
+		if (n < num) {
+			std::cout << "\nElement " << n << ": " << queue[n] << std::endl;
+		}
+
+		else std::cout << "\nElement " << n << " isn't exicting in this queue!\n";
+	}
+
+	else std::cout << "\nQueue is empty!\n";
+}
+
+template <class Qt>
+void Queue<Qt>::show(int *from, int *to)
+{
+	//...
+}
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include "jzQueue.h"
 
+void test1();
+
 int main()
+{
+	test1();
+	system("pause >> NULL");
+	return 0;
+}
+
+void test1()
 {
 	Queue<int> firstQ;
 
 	firstQ.push(25);
-
-	system("pause >> NULL");
-	return 0;
+	firstQ.push(30);
+	firstQ.push(6);
+	firstQ.push(75);
+	firstQ.show();
+	firstQ.show(2);
+	firstQ.show(23);
+	std::cout << std::endl << "Min value: " << firstQ.findMin() << std::endl;
+	std::cout << std::endl << "Number of elements: " << firstQ.size() << std::endl;
+	std::cout << std::endl << "Size of array: " << firstQ.szof() << std::endl; // Doesn't showing the whole size!...
 }
 
